f23433be0b5fce45a11c749da586f6c3d831a406
Brian Bycynski <high5bri@hotmail.com>
2021-08-20 02:56:51 -0400

    rotating navigation

--- PROJECTS/01-expanding-cards/script.js
+++ PROJECTS/01-expanding-cards/script.js
@@ -1,4 +1,17 @@
 //
 //
 
-console.log(123);
+const panels = document.querySelectorAll('.panel');
+
+const removeActiveClasses = function () {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+};
+
+panels.forEach(function (panel, i) {
+  panel.addEventListener('click', function () {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
