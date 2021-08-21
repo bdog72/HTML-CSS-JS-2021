8341a509a9db03f186311a1d0dd27fd6b5726bc9
Brian Bycynski <high5bri@hotmail.com>
2021-08-21 03:09:33 -0400

    Blurry Loading

--- PROJECTS/03-rotating-nav-animation/script.js
+++ PROJECTS/03-rotating-nav-animation/script.js
@@ -1,4 +1,15 @@
 //
 //
 
-console.log(123);
+const open = document.querySelector('#open');
+const close = document.querySelector('#close');
+
+const container = document.querySelector('.container');
+
+open.addEventListener('click', function () {
+  container.classList.add('show-nav');
+});
+
+close.addEventListener('click', function () {
+  container.classList.remove('show-nav');
+});
