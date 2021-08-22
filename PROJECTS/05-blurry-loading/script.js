44ed5e0fdf218cc503fb7632e2ca840e97c75e64
Brian Bycynski <high5bri@hotmail.com>
2021-08-22 02:58:30 -0400

    Finished Blurry Loading Project

--- PROJECTS/05-blurry-loading/script.js
+++ PROJECTS/05-blurry-loading/script.js
@@ -1,28 +1,26 @@
 //
 //
 
-console.log(123);
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
 
-// const loadText = document.querySelector('.loading-text')
-// const bg = document.querySelector('.bg')
+let load = 0;
 
-// let load = 0
+const blurring = function () {
+  load++;
 
-// let int = setInterval(blurring, 30)
+  if (load > 99) {
+    clearInterval(int);
+  }
 
-// function blurring() {
-//   load++
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+};
 
-//   if (load > 99) {
-//     clearInterval(int)
-//   }
-
-//   loadText.innerText = `${load}%`
-//   loadText.style.opacity = scale(load, 0, 100, 1, 0)
-//   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
-// }
+let int = setInterval(blurring, 30);
 
 // // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
-// const scale = (num, in_min, in_max, out_min, out_max) => {
-//   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-// }
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
